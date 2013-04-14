f740d0e99be586a674b8262a33b8daee4e8c824a
Tim Travelstead <travelstead@mac.com>
2013-04-14 13:00:26 -0700

    Really done with Static

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe PagesController do 
 	render_views
 
+	before(:each) do
+		@base_title = "Ruby on Rails Tutorial | "
+	end
+
+
 	describe "get 'home'" do
 		it "should be successful" do
 			get 'home'
@@ -11,7 +16,7 @@ describe PagesController do
 
 		it "should have the right title" do
 			get 'home'
-			response.should have_selector("title", :content => " | Home")
+			response.should have_selector("title", :content => @base_title + "Home")
 		end
 	end
 
@@ -23,7 +28,7 @@ describe PagesController do
 
 		it "should have the right title" do
 			get 'contact'
-			response.should have_selector("title", :content => " | Contact")
+			response.should have_selector("title", :content => @base_title + "Contact")
 		end
 	end
 
@@ -35,7 +40,19 @@ describe PagesController do
 
 		it "should have the right title" do
 			get 'about'
-			response.should have_selector("title", :content => " | About")
+			response.should have_selector("title", :content => @base_title + "About")
+		end
+	end
+
+	describe "get 'help'" do
+		it "should be sucessful" do
+			get 'help'
+			response.should be_success
+		end
+
+		it "should have the right title" do
+			get 'help'
+			response.should have_selector("title", :content => @base_title + "Help")
 		end
 	end
 end
